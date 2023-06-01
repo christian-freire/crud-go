072a9c24fb579db00d7ec55dddfe1abcbf8c1b19
Christian Evangelista Freire <74272121+christian-freire@users.noreply.github.com>
2023-06-01 18:20:47 -0300

    [feat] Error patern and objects

---
+++ go.mod
@@ -0,0 +1,5 @@
+module crud-go
+
+go 1.20
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Println(os.Getenv("TEST"))
 }
